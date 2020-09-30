1d8b101b14ed1e8900d8e905213d40d83249dead
杉森太樹 <sugimori2301@gmail.com>
2020-09-30 11:00:32 +0900

    first commit!

---
+++ README.md
@@ -0,0 +1,41 @@
+# Songle Sync ブラウザ向けプロジェクト
+
+ブラウザで動作するシンプルな Songle Sync アプリケーション例です  
+引数で master/slave が切り替わります
+
+![Songle Sync Browser](ss.png "Songle Sync Browser")
+
+## Songle Sync のサイト上で Token を発行
+
+初めての方は新規登録が必要です  
+以下のURLから新規登録し、`accessToken`と`secretToken`を取得してください
+
+http://api.songle.jp/u/sign_in
+
+## Tokenを設定する
+
+viz.jsの以下の箇所を取得したTokenに書き換える
+
+```javascript
+var accessToken = "YOUR_ACCESS_TOKEN"
+var secretToken = "YOUR_SECRET_TOKEN"
+```
+
+## masterでアクセスする
+
+`index.html` を引数 `master=1` を付けてブラウザで開いてください。  
+右下に動画が表示され、再生・停止・先頭へシーク・サビ出しが出来ます。  
+
+```
+[ファイルパス]/index.html?master=1
+```
+
+## slaveでアクセスする
+
+別ウィンドウで `index.html` をブラウザで開いてください。  
+masterで再生される動画に合わせてバーやコード名が変化します。  
+同じ`accessToken`であれば別のPCでもmasterに同期して動作します。
+
+```
+[ファイルパス]/index.html
+```
---
+++ viz.js
@@ -0,0 +1,172 @@
+/**:::::::::::::::::::::::::::::::::::::::::::::::::
+ * github URL
+ * https://github.com/SongleJp/songle-sync-app-browser
+ * 
+ * 
+ * 【システムの流れ】
+ * 
+ * 1,ユーザが曲を選ぶ(再生したいメディアを選択)
+ * 2,再生したいメディアのURL、MMDデータが飛ぶ
+ * 3,マスター、ユーザによって処理を分岐
+ * 		マスター：曲、動画を再生(ステージのつもり)
+ * 		ユーザ：スマホの画面に演出を表示
+ * 4,曲が終わったら1に戻る
+ * 
+ * 【それぞれ出来ること】
+ * マスター：動画再生、演出再生
+ * 会場のユーザ：曲の選択、演出画面の表示、チャットの送信
+ * オンラインのユーザ：演出画面の表示、チャットの送信
+ :::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+let media_url = "";
+
+// 再生したいメディアを指定
+//DECORATOR
+//media_url = "https://www.youtube.com/watch?v=zweVJrnE1uY";
+
+//ダンスロボットダンス
+//media_url = "https://www.nicovideo.jp/watch/sm30177801";
+
+//砂の惑星
+media_url = "https://www.youtube.com/watch?v=AS4q9yaWJkI";
+
+
+// Tokenを指定(以下のURLからTokenを発行できます)
+// http://api.songle.jp/user/
+var accessToken = "000000c8-4jMshRb";
+var secretToken = "WKDaG2iyYATCLc3TSE3gHBVfbW1oz5rX";
+
+var player;
+
+// APIの準備が出来ると呼ばれる
+window.onSongleWidgetAPIReady = function(SongleWidgetAPI){
+	window.SW = SongleWidgetAPI;
+	SW.System.defaultEndpointWebClientProtocol = "https:"; 
+	init();
+}
+
+window.init = function(){
+	if ( getUrlVars().master == "1" ) {
+		// masterの場合 (引数 master=1)
+		player = new SW.Player({
+			mediaElement: "div#widget" // プレイヤーを埋め込むDOMを指定
+		});
+		player.accessToken = accessToken;
+		player.secretToken = secretToken; // secretTokenをセットするとmasterになる
+		// 再生するメディアをセット
+		console.log(media_url);
+		player.useMedia( media_url ); 
+	}
+	else {
+		// slaveの場合
+		player = new SW.Player();
+		player.accessToken = accessToken;
+	}
+
+	// slaveを同期させるプラグインを設定
+	player.addPlugin(new SW.Plugin.SongleSync());
+
+	// 利用するイベントのプラグインを設定
+	player.addPlugin(new SW.Plugin.Beat());
+	player.addPlugin(new SW.Plugin.Chorus());
+	player.addPlugin(new SW.Plugin.Chord());
+
+	// 各イベントに対応するアクションを設定
+	setBeatEvent();
+	setChordEvent();
+	setChorusEvent();
+
+	// masterの場合は動画を再生
+	if (getUrlVars().master == "1" ) {
+		// mediaReadyで動画が準備完了したら実行
+		player.on("mediaReady", function(){
+			// プレイヤー操作ボタン設定	
+			setPlayerCtrl();
+			// 自動再生
+			// setTimeout(function(){
+			// 	player.play();
+			// }, 1000);
+		})
+	}
+}
+
+// プレイヤー操作ボタン設定
+window.setPlayerCtrl = function(){
+	// 再生
+	$("#widget_ctrl .play").click( function(){
+		console.log(media_url);
+		player.play();
+	});
+	// 停止
+	$("#widget_ctrl .pause").click( function(){
+		player.pause();
+	});
+	// 先頭
+	$("#widget_ctrl .head").click( function(){
+		player.seekTo(0);
+	});
+	// サビ出し
+	$("#widget_ctrl .seekto_chorus").click( function(){
+		player.seekToNextChorusSectionItem()
+	});
+	$("#widget_ctrl").css({ display: "table" });
+	$(".memo").show();
+}
+
+// ビートでタイルの色を変える（cssで指定）
+window.setBeatEvent = function(){
+	player.on( "beatEnter", function(e){
+		for(var i=1; i<=4; i++ ) {
+			li = $("#beats li.b" + i )
+			if (e.data.beat.number == i) {
+				li.addClass("current");
+				console.log(e.data.beat.number + "beat!")
+			} else {
+				li.removeClass("current");
+			}
+		}
+	});
+}
+
+// コード左上に表示する
+window.setChordEvent = function(){
+	player.on( "chordEnter", function(e){
+		if (e.data.chord.name != "N") {
+			$("#chord").text( e.data.chord.name );
+		} else {
+			$("#chord").text( "" );			
+		}
+	});
+}
+
+// サビはビートの色を変更(cssで指定)し、右上に「サビ」と表示させる
+window.setChorusEvent = function(){
+	player.on( "chorusSectionEnter", function(e){
+		$("#beats").addClass("chorus");
+		$("#chorus_alert").show();
+	});
+	player.on( "chorusSectionLeave", function(e){
+		$("#beats").removeClass("chorus");
+		$("#chorus_alert").hide();		
+	});	
+}
+
+// URLの引数を取得する関数
+window.getUrlVars = function() {
+	var i, key, keySearch, len, p, param, val, vars;
+	vars = {};
+	param = location.search.substring(1).split('&');
+	for (i = 0, len = param.length; i < len; i++) {
+		p = param[i];
+		keySearch = p.search(/=/);
+		key = '';
+		if (keySearch !== -1) {
+			key = p.slice(0, keySearch);
+			val = p.slice(p.indexOf('=', 0) + 1);
+			if (key !== '') {
+				vars[key] = decodeURI(val);
+			}
+		}
+	}
+	return vars;
+}
